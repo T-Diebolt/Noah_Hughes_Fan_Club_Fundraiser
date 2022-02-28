7ff2c7013963a0c540b8c23818d1bb0cac1744f8
T-Diebolt <theodieb542@outlook.com>
2022-02-28 14:13:15 -0500

    Add project files.

---
+++ Noah_Hughes_Fan_Club_Fundraiser/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Noah_Hughes_Fan_Club_Fundraiser
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.amateurPhotosInput = new System.Windows.Forms.TextBox();
+            this.amateurPhotosLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.expertPhotosInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sculpturesInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.calculateFormat = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.calculateOutput = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.receiptOutput = new System.Windows.Forms.Label();
+            this.receiptTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.LavenderBlush;
+            this.titleLabel.Font = new System.Drawing.Font("Harlow Solid Italic", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Black;
+            this.titleLabel.Location = new System.Drawing.Point(-11, -2);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(811, 82);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Noah Hughes Fan Club Fundraiser";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.Color.White;
+            this.receiptLabel.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptLabel.Location = new System.Drawing.Point(496, 94);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(283, 418);
+            this.receiptLabel.TabIndex = 1;
+            this.receiptLabel.Visible = false;
+            // 
+            // amateurPhotosInput
+            // 
+            this.amateurPhotosInput.BackColor = System.Drawing.Color.LavenderBlush;
+            this.amateurPhotosInput.Font = new System.Drawing.Font("Bell MT", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amateurPhotosInput.Location = new System.Drawing.Point(227, 98);
+            this.amateurPhotosInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.amateurPhotosInput.Name = "amateurPhotosInput";
+            this.amateurPhotosInput.Size = new System.Drawing.Size(137, 40);
+            this.amateurPhotosInput.TabIndex = 2;
+            this.amateurPhotosInput.Text = "0";
+            this.amateurPhotosInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // amateurPhotosLabel
+            // 
+            this.amateurPhotosLabel.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amateurPhotosLabel.Location = new System.Drawing.Point(12, 94);
+            this.amateurPhotosLabel.Name = "amateurPhotosLabel";
+            this.amateurPhotosLabel.Size = new System.Drawing.Size(196, 54);
+            this.amateurPhotosLabel.TabIndex = 3;
+            this.amateurPhotosLabel.Text = "Amatauer Photos of Noah: $2.49 ea.";
+            this.amateurPhotosLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.amateurPhotosLabel.Click += new System.EventHandler(this.amateurPhotosLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 54);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Expert Photos of Noah: $7.49 ea.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // expertPhotosInput
+            // 
+            this.expertPhotosInput.BackColor = System.Drawing.Color.LavenderBlush;
+            this.expertPhotosInput.Font = new System.Drawing.Font("Bell MT", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.expertPhotosInput.Location = new System.Drawing.Point(227, 174);
+            this.expertPhotosInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.expertPhotosInput.Name = "expertPhotosInput";
+            this.expertPhotosInput.Size = new System.Drawing.Size(137, 40);
+            this.expertPhotosInput.TabIndex = 5;
+            this.expertPhotosInput.Text = "0";
+            this.expertPhotosInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-1, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 54);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fan Made Sculptures of Noah: $19.99 ea.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // sculpturesInput
+            // 
+            this.sculpturesInput.BackColor = System.Drawing.Color.LavenderBlush;
+            this.sculpturesInput.Font = new System.Drawing.Font("Bell MT", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sculpturesInput.Location = new System.Drawing.Point(227, 249);
+            this.sculpturesInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sculpturesInput.Name = "sculpturesInput";
+            this.sculpturesInput.Size = new System.Drawing.Size(137, 40);
+            this.sculpturesInput.TabIndex = 7;
+            this.sculpturesInput.Text = "0";
+            this.sculpturesInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Harlow Solid Italic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(4, 322);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(204, 54);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate Total";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // calculateFormat
+            // 
+            this.calculateFormat.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateFormat.Location = new System.Drawing.Point(13, 389);
+            this.calculateFormat.Name = "calculateFormat";
+            this.calculateFormat.Size = new System.Drawing.Size(103, 132);
+            this.calculateFormat.TabIndex = 9;
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.BackColor = System.Drawing.Color.LavenderBlush;
+            this.tenderedInput.Font = new System.Drawing.Font("Bell MT", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedInput.Location = new System.Drawing.Point(371, 329);
+            this.tenderedInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(119, 40);
+            this.tenderedInput.TabIndex = 10;
+            this.tenderedInput.Text = "0";
+            this.tenderedInput.Visible = false;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.Font = new System.Drawing.Font("Harlow Solid Italic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(227, 322);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(139, 54);
+            this.tenderedLabel.TabIndex = 11;
+            this.tenderedLabel.Text = "Pay with:";
+            this.tenderedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tenderedLabel.Visible = false;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Font = new System.Drawing.Font("Harlow Solid Italic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(227, 373);
+            this.changeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(264, 44);
+            this.changeButton.TabIndex = 12;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Visible = false;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // calculateOutput
+            // 
+            this.calculateOutput.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateOutput.Location = new System.Drawing.Point(112, 390);
+            this.calculateOutput.Name = "calculateOutput";
+            this.calculateOutput.Size = new System.Drawing.Size(87, 132);
+            this.calculateOutput.TabIndex = 13;
+            this.calculateOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(228, 420);
+            this.changeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(120, 89);
+            this.changeLabel.TabIndex = 14;
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeOutput.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.changeOutput.Location = new System.Drawing.Point(371, 420);
+            this.changeOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(120, 89);
+            this.changeOutput.TabIndex = 15;
+            this.changeOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Font = new System.Drawing.Font("Harlow Solid Italic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(372, 174);
+            this.receiptButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(120, 117);
+            this.receiptButton.TabIndex = 16;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Visible = false;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // receiptOutput
+            // 
+            this.receiptOutput.BackColor = System.Drawing.Color.White;
+            this.receiptOutput.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptOutput.Location = new System.Drawing.Point(707, 94);
+            this.receiptOutput.Name = "receiptOutput";
+            this.receiptOutput.Size = new System.Drawing.Size(73, 418);
+            this.receiptOutput.TabIndex = 17;
+            this.receiptOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.receiptOutput.Visible = false;
+            // 
+            // receiptTitle
+            // 
+            this.receiptTitle.BackColor = System.Drawing.Color.White;
+            this.receiptTitle.Font = new System.Drawing.Font("Bell MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptTitle.Location = new System.Drawing.Point(497, 94);
+            this.receiptTitle.Name = "receiptTitle";
+            this.receiptTitle.Size = new System.Drawing.Size(283, 47);
+            this.receiptTitle.TabIndex = 18;
+            this.receiptTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.receiptTitle.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Harlow Solid Italic", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(372, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 42);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepPink;
+            this.ClientSize = new System.Drawing.Size(791, 519);
+            this.Controls.Add(this.receiptTitle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.receiptOutput);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.calculateOutput);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.calculateFormat);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.sculpturesInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.expertPhotosInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.amateurPhotosLabel);
+            this.Controls.Add(this.amateurPhotosInput);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "form1";
+            this.Text = "Noah Hughes Fan Club Fundraiser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.TextBox amateurPhotosInput;
+        private System.Windows.Forms.Label amateurPhotosLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox expertPhotosInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sculpturesInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label calculateFormat;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label calculateOutput;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label receiptOutput;
+        private System.Windows.Forms.Label receiptTitle;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Noah_Hughes_Fan_Club_Fundraiser/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Noah_Hughes_Fan_Club_Fundraiser
+{
+    public partial class form1 : Form
+    {
+        //esablish variables
+        double amateurPhotosPrice = 2.5;
+        double expertPhotosPrice = 7.5;
+        double sculpturePrice = 20;
+        int numAmateurPhotos;
+        int numExpertPhotos;
+        int numSculptures;
+        double amateurPhotos;
+        double expertPhotos;
+        double sculptures;
+        double subTotal;
+        double taxRate = 0.13;
+        double tax;
+        double total;
+        double tendered;
+        double change;
+
+        //establish soundplayer
+        SoundPlayer receiptSound = new SoundPlayer(Properties.Resources.Receipt);
+
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void amateurPhotosLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                //establish variables
+                numAmateurPhotos = Convert.ToInt32(amateurPhotosInput.Text);
+                numExpertPhotos = Convert.ToInt32(expertPhotosInput.Text);
+                numSculptures = Convert.ToInt32(sculpturesInput.Text);
+
+                amateurPhotos = Convert.ToDouble(amateurPhotosInput.Text) * amateurPhotosPrice;
+                expertPhotos = Convert.ToDouble(expertPhotosInput.Text) * expertPhotosPrice;
+                sculptures = Convert.ToDouble(sculpturesInput.Text) * sculpturePrice;
+
+                subTotal = amateurPhotos + expertPhotos + sculptures;
+                tax = subTotal * taxRate;
+                total = subTotal + tax;
+
+                //make tendered visible
+                tenderedInput.Visible = true;
+                tenderedLabel.Visible = true;
+                changeButton.Visible = true;
+
+                //Display totals
+                calculateOutput.Text = $"{subTotal.ToString("C")}\n\n{tax.ToString("C")}\n\n{total.ToString("C")}";
+                calculateFormat.Text = "Subtotal\n\nTax\n\nTotal";
+               
+
+            }
+            catch 
+            {
+                calculateFormat.Text = "ERROR";
+            }
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //establish new variables
+                tendered = Convert.ToDouble(tenderedInput.Text);
+                change = tendered - total;
+
+                //output message
+                changeLabel.Text = "Cash\n\nChange";
+                changeOutput.Text = $"{tendered.ToString("C")}\n\n{change.ToString("C")}";
+                
+                //make receipt visible
+                receiptButton.Visible = true;
+                
+            }
+            catch 
+            {
+                changeLabel.Text = "ERROR";
+            }
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            
+            //make receipt appear, play sound
+            receiptOutput.Visible = true;
+            receiptLabel.Visible = true;
+            receiptTitle.Visible = true;
+            receiptSound.Play();
+            Refresh();
+            Thread.Sleep(500);
+
+            //print receipt line #1
+            receiptTitle.Text = "Noah Hughes Fan Club";
+            Refresh();
+            Thread.Sleep(650);
+
+
+            //print receipt line #2
+            receiptLabel.Text = $"\n\nAmateur Photos X{numAmateurPhotos}";
+            receiptOutput.Text = $"\n\n{amateurPhotos.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #3
+            receiptLabel.Text += $"\nEexpert Photos X{numExpertPhotos}";
+            receiptOutput.Text += $"\n{expertPhotos.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #4
+            receiptLabel.Text += $"\nSculptures X{numSculptures}";
+            receiptOutput.Text += $"\n{sculptures.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #5
+            receiptLabel.Text += $"\n\nSubtotal";
+            receiptOutput.Text += $"\n\n{subTotal.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #6
+            receiptLabel.Text += $"\nTax";
+            receiptOutput.Text += $"\n{tax.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #7
+            receiptLabel.Text += $"\nTotal";
+            receiptOutput.Text += $"\n{total.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #8
+            receiptLabel.Text += $"\n\nTendered";
+            receiptOutput.Text += $"\n\n{tendered.ToString("C")}";
+            Refresh();
+            Thread.Sleep(650);
+
+            //print receipt line #9
+            receiptLabel.Text += $"\nChange";
+            receiptOutput.Text += $"\n{change.ToString("C")}";
+            Refresh();
+            Thread.Sleep(2250);
+
+            //print receipt line #10
+            receiptLabel.Text += $"\n\n               FIND NOAH";
+            Refresh();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //clears order
+            amateurPhotosInput.Text = "0";
+            expertPhotosInput.Text = "0";
+            sculpturesInput.Text = "0";
+            calculateFormat.Text = "";
+            calculateOutput.Text = "";
+            tenderedLabel.Visible = false;
+            tenderedInput.Visible = false;
+            tenderedInput.Text = "0";
+            changeLabel.Text = "";
+            changeOutput.Text = "";
+            changeButton.Visible = false;
+            receiptButton.Visible = false;
+            receiptLabel.Text = "";
+            receiptOutput.Text = "";
+            receiptTitle.Text = "";
+            receiptLabel.Visible = false;
+            receiptOutput.Visible = false;
+            receiptTitle.Visible = false;
+
+
+        }
+    }
+}
